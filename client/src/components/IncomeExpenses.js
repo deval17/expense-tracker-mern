e51312daa1cdfc44e0b42f5900f57bbb95ebbd5c
Deval Modi <devalmodi1712@gmail.com>
2020-08-20 00:03:51 +0530

    Expense Tracker with MERN.

--- client/src/components/IncomeExpenses.js
+++ client/src/components/IncomeExpenses.js
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react'
 import {GlobalContext} from '../context/GlobalState';
+import {numberWithCommas} from '../utils/format';
 
 export const IncomeExpenses = () => {
   const {transactions} = useContext(GlobalContext);
@@ -20,11 +21,11 @@ export const IncomeExpenses = () => {
         <div className="inc-exp-container">
         <div>
           <h4>Income</h4>
-    <p className="money plus"><b>₹{income}</b></p>
+    <p className="money plus"><b>₹{numberWithCommas(income)}</b></p>
         </div>
         <div>
           <h4>Expense</h4>
-    <p className="money minus"><b>₹{expense}</b></p>
+    <p className="money minus"><b>₹{numberWithCommas(expense)}</b></p>
         </div>
       </div>
     )
